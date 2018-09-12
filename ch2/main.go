70bac935485e7b21594a57e9257912280effd39e
Jason Mojica <mojicaj@gmail.com>
2018-09-12 10:53:02 -0400

    Chapter 2 exercises

---
+++ ch2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	// fmt.Println("1 + 1 =", 1+1)
+	// fmt.Println("1 + 1 =", 1.0+1.0)
+	// fmt.Println(len("Hello, World"))
+	// fmt.Println("Hello, World"[1])
+	// fmt.Println("Hello, " + "World")
+	fmt.Println("32,132 x 42,452 =", 32132*42452)
+}
